2f4bc4355552fa17fb38b674d514f2dfa1dd36b3
TamirShalit <shalit.tamir@gmail.com>
2018-08-28 00:47:27 +0300

    Initial setup.py and package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytest-web-results',
+    version='0.1',
+    packages=find_packages(exclude=['tests']),
+    url='https://github.com/TamirShalit/pytest-web-results',
+    install_requires=['pytest'],
+    author='Tamir Shalit',
+    author_email='shalit.tamir@gmail.com',
+    description='Pytest plugin for viewing results via web server'
+)
