33c6f1f838a6b5da6eb811be07d7cc7665a7091c
TamirShalit <shalit.tamir@gmail.com>
2018-09-23 11:35:05 +0300

    Change to use temporary DB file

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,13 +5,21 @@ from webresultserver import app_factory
 
 pytest_plugins = 'pytester'
 
-CONFIG_LOCATION = os.path.join(os.path.dirname(__file__), 'test_config.json')
+CONFIG_TEMPLATE_LOCATION = os.path.join(os.path.dirname(__file__), 'test_config.template')
 
 
 @pytest.fixture
-def app():
+def app(tmpdir):
+    """
+    :type tmpdir: py._path.local.LocalPath
+    """
+    db_location = tmpdir.join('test.db')
+    with open(CONFIG_TEMPLATE_LOCATION) as config_template_file:
+        config_content = config_template_file.read().format(db_location=db_location.strpath)
+    config_location = tmpdir.join('test_config.json')
+    config_location.write(config_content)
     try:
-        os.environ[app_factory.CONFIG_LOCATION_ENVIRONMENT_VARIABLE] = CONFIG_LOCATION
+        os.environ[app_factory.CONFIG_LOCATION_ENVIRONMENT_VARIABLE] = config_location.strpath
         flask_app = app_factory.create_app(__name__)
 
         with flask_app.app_context():
